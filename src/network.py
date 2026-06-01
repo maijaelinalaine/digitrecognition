fc5b7c867c35633bf93161a1d278db0736ea955b
maijaelinalaine <maijaelina05@gmail.com>
2026-06-01 20:10:14 +0300

    finish neural network

--- src/network.py
+++ src/network.py
@@ -1,19 +1,39 @@
 import numpy as np
+import random
 
 class Network:
 
-    def __init__(self, sizes, rng=None):
-        if rng is None:
-            rng = np.random.default_rng()
+    def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x)
+                        for x, y in zip(sizes[:-1], sizes[1:])]
 
-        self.weights = [
-            rng.standard_normal((y, x)) * np.sqrt(2.0 / x)
-            for x, y in zip(sizes[:-1], sizes[1:])
-        ]
+    def feedforward(self, a):
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a) + b)
+        return a
 
-        self.biases = [np.zeros(y) for y in sizes[1:]]
+    def SGD(self, training_data, epochs, batch_size, eta):
+        for _ in range(epochs):
+            random.shuffle(training_data)
+            batches = [training_data[k:k + batch_size]
+                            for k in range(0, len(training_data), batch_size)]
+            for batch in batches:
+                self.update_batch(batch, eta)
+
+    def update_batch(self, batch, eta):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        for x, y in batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+            nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+            nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+        self.weights = [w-(eta/len(batch))*nw
+                        for w, nw in zip(self.weights, nabla_w)]
+        self.biases = [b-(eta/len(batch))*nb
+                    for b, nb in zip(self.biases, nabla_b)]
 
     def backprop(self, x, y):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
@@ -23,7 +43,7 @@ class Network:
         activations = [x] # list to store all the activations, layer by layer
         zs = [] # list to store all the z vectors, layer by layer
         for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, activation)+b
+            z = np.dot(w, activation) + b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
@@ -36,16 +56,16 @@ class Network:
         for l in range(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
             nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
         return nabla_b, nabla_w
 
     def cost_derivative(self, output_activations, y):
-        return output_activations-y
+        return output_activations - y
 
 def sigmoid(z):
-    return 1.0/(1.0+np.exp(-z))
+    return 1.0 / (1.0 + np.exp(-z))
 
 def sigmoid_prime(z):
     # derivative of sigmoid function
